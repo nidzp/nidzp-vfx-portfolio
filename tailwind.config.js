ed3d9b6ba9158b43cb1d20354cc91f042ab4e8a4
Nikola <95056678+nidzp@users.noreply.github.com>
2025-06-15 05:42:21 +0200

    fix: tailwind setup, enable styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
